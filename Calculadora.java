13084503ddd4dcbc93c3533252a5735de889dbca
Daniel vieira ornelas <67751429+Daniel479@users.noreply.github.com>
2024-11-22 16:30:40 -0300

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,7 +1,18 @@
 package com.simulandotestes;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Calculadora {
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public int dividir(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Não pode dividir por zero");
+        }
+        return a / b;
     }
 }
